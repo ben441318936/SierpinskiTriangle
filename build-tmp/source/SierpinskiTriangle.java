a1e85c1aca324e1722a77e7592aabe18b6ed0bcf
Jingbin Huang <ben441318936@gmail.com>
2015-01-12 21:48:28 -0800

    adjusted size

--- build-tmp/source/SierpinskiTriangle.java
+++ build-tmp/source/SierpinskiTriangle.java
@@ -31,7 +31,7 @@ public void mouseDragged()//optional
 }
 public void sierpinski(double x, double y, double len) 
 {
-	if(len>100)
+	if(len>10)
 	{
 		sierpinski(x,y,len/2);
 		sierpinski(x+len/2,y,len/2);
